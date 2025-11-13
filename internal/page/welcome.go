66590dbf5dbb7031f14980386bab6f2fa7fca88a
NeilClack <cnclack@pm.me>
2025-11-13 11:09:30 -0600

    Display email and license key on welcome page

--- internal/page/welcome.go
+++ internal/page/welcome.go
@@ -18,10 +18,14 @@ var GitVersion = "dev"
 
 type Welcome struct {
 	Base
-	title   StringItem
-	version StringItem
-	prompt  StringItem
-	note    StringItem
+	title              StringItem
+	version            StringItem
+	prompt             StringItem
+	note               StringItem
+	licenseInfoHeading StringItem
+	emailDisplay       StringItem
+	licenseKeyDisplay  StringItem
+	hasLicenseInfo     bool
 }
 
 // Init performs basic page initialization and creation of static assets.
@@ -70,6 +74,46 @@ func (p *Welcome) Init(app ApplicationInterface) error {
 		return fmt.Errorf("unable to create note string texture: %w", err)
 	}
 
+	// Load license information from database
+	emailAddress, err := db.GetConfigValue("email_address")
+	if err != nil {
+		return fmt.Errorf("retrieving email_address from db: %w", err)
+	}
+
+	licenseKey, err := db.GetConfigValue("license_key")
+	if err != nil {
+		return fmt.Errorf("retrieving license_key from db: %w", err)
+	}
+
+	// Create license info textures if data exists
+	if emailAddress != "" || licenseKey != "" {
+		p.hasLicenseInfo = true
+
+		// Create heading
+		p.licenseInfoHeading, err = NewStringItem("License Info", font, colors.White)
+		if err != nil {
+			return fmt.Errorf("unable to create license info heading texture: %w", err)
+		}
+
+		// Create email display if exists
+		if emailAddress != "" {
+			emailText := fmt.Sprintf("Email: %s", emailAddress)
+			p.emailDisplay, err = NewStringItem(emailText, font, colors.White)
+			if err != nil {
+				return fmt.Errorf("unable to create email display texture: %w", err)
+			}
+		}
+
+		// Create license key display if exists
+		if licenseKey != "" {
+			licenseKeyText := fmt.Sprintf("License Key: %s", licenseKey)
+			p.licenseKeyDisplay, err = NewStringItem(licenseKeyText, font, colors.White)
+			if err != nil {
+				return fmt.Errorf("unable to create license key display texture: %w", err)
+			}
+		}
+	}
+
 	// Get base font size from database
 	baseFontSizeStr, err := db.GetConfigValue("init_font_size")
 	if err != nil {
@@ -84,6 +128,18 @@ func (p *Welcome) Init(app ApplicationInterface) error {
 	fontSize := int32(32)
 	scale := float32(fontSize) / float32(baseFontSize)
 	p.note.Scale(scale * 0.75)
+
+	// Scale license info texts
+	if p.hasLicenseInfo {
+		licenseInfoScale := float32(28) / float32(baseFontSize)
+		p.licenseInfoHeading.Scale(licenseInfoScale)
+		if p.emailDisplay.ID != 0 {
+			p.emailDisplay.Scale(licenseInfoScale * 0.85)
+		}
+		if p.licenseKeyDisplay.ID != 0 {
+			p.licenseKeyDisplay.Scale(licenseInfoScale * 0.85)
+		}
+	}
 
 	return nil
 }
@@ -159,7 +215,61 @@ func (p *Welcome) Render() error {
 	titlePos := mgl32.Vec2{float32(titleX), float32(titleY)}
 	versionPos := mgl32.Vec2{float32(versionX), float32(versionY)}
 
-	// 6) Get shader and render
+	// 6) Position license info at bottom
+	if p.hasLicenseInfo {
+		licenseInfoMargin := int32(10)
+		bottomMargin := int32(20)
+
+		// License info items are already scaled in Init
+		licenseHeadingDim := p.licenseInfoHeading.RawDimensions()
+		licenseHeadingW, licenseHeadingH := licenseHeadingDim[0], licenseHeadingDim[1]
+
+		var emailDisplayH, licenseKeyDisplayH float32
+		if p.emailDisplay.ID != 0 {
+			emailDisplayDim := p.emailDisplay.RawDimensions()
+			emailDisplayH = emailDisplayDim[1]
+		}
+		if p.licenseKeyDisplay.ID != 0 {
+			licenseKeyDisplayDim := p.licenseKeyDisplay.RawDimensions()
+			licenseKeyDisplayH = licenseKeyDisplayDim[1]
+		}
+
+		// Calculate total height of license info block
+		licenseInfoTotalH := int32(licenseHeadingH)
+		if p.emailDisplay.ID != 0 {
+			licenseInfoTotalH += int32(emailDisplayH) + licenseInfoMargin
+		}
+		if p.licenseKeyDisplay.ID != 0 {
+			licenseInfoTotalH += int32(licenseKeyDisplayH) + licenseInfoMargin
+		}
+
+		// Start from bottom and work up
+		currentY := bottomMargin
+
+		// License key (bottom-most if it exists)
+		if p.licenseKeyDisplay.ID != 0 {
+			licenseKeyDisplayDim := p.licenseKeyDisplay.RawDimensions()
+			licenseKeyDisplayW := licenseKeyDisplayDim[0]
+			p.licenseKeyDisplay.X = p.Base.ScreenCenterX - int32(licenseKeyDisplayW/2)
+			p.licenseKeyDisplay.Y = currentY
+			currentY += int32(licenseKeyDisplayH) + licenseInfoMargin
+		}
+
+		// Email
+		if p.emailDisplay.ID != 0 {
+			emailDisplayDim := p.emailDisplay.RawDimensions()
+			emailDisplayW := emailDisplayDim[0]
+			p.emailDisplay.X = p.Base.ScreenCenterX - int32(emailDisplayW/2)
+			p.emailDisplay.Y = currentY
+			currentY += int32(emailDisplayH) + licenseInfoMargin
+		}
+
+		// Heading (top-most)
+		p.licenseInfoHeading.X = p.Base.ScreenCenterX - int32(licenseHeadingW/2)
+		p.licenseInfoHeading.Y = currentY
+	}
+
+	// 7) Get shader and render
 	textShader, ok := sm.Get("text")
 	if !ok {
 		return fmt.Errorf("failed to fetch text shader")
@@ -171,6 +281,17 @@ func (p *Welcome) Render() error {
 	p.Base.RenderTexture(textShader, p.prompt.ID, p.prompt.Position(), promptDim, color)
 	p.Base.RenderTexture(textShader, p.note.ID, p.note.Position(), p.note.RawDimensions(), color)
 
+	// Render license info if it exists
+	if p.hasLicenseInfo {
+		p.Base.RenderTexture(textShader, p.licenseInfoHeading.ID, p.licenseInfoHeading.Position(), p.licenseInfoHeading.RawDimensions(), color)
+		if p.emailDisplay.ID != 0 {
+			p.Base.RenderTexture(textShader, p.emailDisplay.ID, p.emailDisplay.Position(), p.emailDisplay.RawDimensions(), color)
+		}
+		if p.licenseKeyDisplay.ID != 0 {
+			p.Base.RenderTexture(textShader, p.licenseKeyDisplay.ID, p.licenseKeyDisplay.Position(), p.licenseKeyDisplay.RawDimensions(), color)
+		}
+	}
+
 	return nil
 }
 
@@ -181,5 +302,16 @@ func (p *Welcome) Destroy() error {
 	gl.DeleteTextures(1, &p.version.ID)
 	gl.DeleteTextures(1, &p.prompt.ID)
 	gl.DeleteTextures(1, &p.note.ID)
+
+	if p.hasLicenseInfo {
+		gl.DeleteTextures(1, &p.licenseInfoHeading.ID)
+		if p.emailDisplay.ID != 0 {
+			gl.DeleteTextures(1, &p.emailDisplay.ID)
+		}
+		if p.licenseKeyDisplay.ID != 0 {
+			gl.DeleteTextures(1, &p.licenseKeyDisplay.ID)
+		}
+	}
+
 	return p.Base.Destroy()
 }
