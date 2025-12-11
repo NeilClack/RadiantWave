63c4aacd51e6b1ecff9c15f94ba5e45fa3bd2ecb
NeilClack <cnclack@pm.me>
2025-12-11 13:35:35 -0600

    Auto update on start
    We can skip trying to create a "check for updates" button within the program. By forcing the updater script to run as part of the startup script, then we get automatic updates via CTRL+Q, which kills the program, therefore restarting the getty session, therefore re-running the script and checking for and installing updates! The updater script then restarts getty anyways, so the new version gets run without ever needing a reboot. How fucking cheeky and clever is that?

--- system/etc/profile.d/radiantwave-kiosk.sh
+++ system/etc/profile.d/radiantwave-kiosk.sh
@@ -2,5 +2,9 @@
 if [ -z "$WAYLAND_DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     export XDG_RUNTIME_DIR=/run/user/$(id -u)
     export SDL_VIDEODRIVER=wayland
+    
+    # Check for updates before launching
+    /usr/bin/python3 /usr/local/bin/radiantwave-updater.py || true
+    
     exec cage -- /usr/local/bin/radiantwave
 fi
